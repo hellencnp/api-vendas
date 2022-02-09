03af260c42130219ca3a6bb0cadc657738bc2119
hellencnp <hellen.nasci@hotmail.com>
2022-02-09 09:11:02 -0300

    criando servidor web

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('Funcionou nosso projeto Node com Ts')
+import express from 'express'
+//cria um servidor web ou http express
+let server = express()
+
+//converte os dados do usuario em json
+server.use(express.json())
+
+server.listen(33333,()=>{
+    console.log('Servidor iniciado')
+})
